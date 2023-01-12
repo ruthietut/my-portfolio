cb5bd07f34a99b8c6669a8b81f77eaa18fa828c8
Ruth Obasi <88226737+ruthietut@users.noreply.github.com>
2023-01-12 13:13:35 +0100

    updated workCardData

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -8,26 +8,28 @@ const WorkCardData = [
     title: "A funtional Calculator",
     text: "I built this calculator using react with mathematical operations such as 'Addition,multiplication,division and substraction' as a project in Altschool Africa. ",
     view: "https://React-Calculator.ruthietut.repl.co",
-    Source: "https://thealtschool.com",
+    Source: "https://github.com/ruthietut/React-Calculator",
   },
   {
     imgsrc: project2,
     title: "network provider identification",
     text: "This application performs a check on the network provider of a phone number, returns the network provider name and display the network provider logo. A holiday challenge project in Altschool Africa.",
     view: "https://holiday-hackathon.ruthietut.repl.co",
-    source: "https://thealtschool.com",
+    source: "https://github.com/ruthietut/TopGs",
   },
   {
     imgsrc: project3,
     title: "My portfolio",
     text: "This is my portfolio website. I built it using react, styled with css and hosted on netlify. it has a home page, a project page and about me page and a contact page. each page is dynamic and functional with dynamic cards created using props.",
     view: "https://ruthobasi.netlify.app/",
-    Source: "https://thealtschool.com",
+    Source: "https://github.com/ruthietut/my-portfolio",
   },
   {
     imgsrc: project4,
     title: "Altschool semester exam",
     text: "Setup react-router, implement Nested routes, 404 page, and Error boundary. Set up fake userAuthContext using the context API to always carry out a fake authentication, bonus - extract out a custom hook to get the currently logged-in user. Implement SEO and Navigation menu that will show on each page.",
+    view:"https://altschool-second-semester.web.app/",
+    source:"https://github.com/ruthietut/Altschool-second-semester-exam",
   }
 ];
 
